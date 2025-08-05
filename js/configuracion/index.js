c5e82c5d33f495c692022d11e22eab4733b187ec
Carloschiloe <147261982+Carloschiloe@users.noreply.github.com>
2025-08-04 21:53:53 -0400

    Update index.js

--- js/configuracion/index.js
+++ js/configuracion/index.js
@@ -1,6 +1,6 @@
 import { renderCriteriosClasificacion } from './configuracion_criterios.js';
 // Importa la tabla de proveedores directamente desde la carpeta correcta
-import { initTablaProveedores } from './proveedores/tabla.js';
+import { initTablaProveedores } from './Proveedores/tabla.js';
 // Cuando tengas listos los otros módulos, los agregas igual:
 /// import { renderClientes } from './configuracion_clientes.js';
 /// import { renderEmpresasTransporte } from './configuracion_transportes.js';
@@ -35,4 +35,5 @@ document.addEventListener('DOMContentLoaded', () => {
   // Mostrar por defecto la primera pestaña
   loadTab('tab-criterios');
 });
+
 
