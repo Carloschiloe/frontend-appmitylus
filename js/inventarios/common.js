36a642a6a8d680ca8dbfeb04d6286d007f143cca
Carloschiloe <147261982+Carloschiloe@users.noreply.github.com>
2025-08-23 21:00:32 -0400

    Delete js directory

--- js/inventarios/common.js
+++
@@ -1,6 +0,0 @@
-// Helpers compartidos para todos los inventarios futuros
-export function isoNowLocal() {
-  const d = new Date();
-  d.setMinutes(d.getMinutes() - d.getTimezoneOffset());
-  return d.toISOString().slice(0,16); // yyyy-MM-ddTHH:mm
-}
